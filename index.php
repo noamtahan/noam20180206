f667db969121476ff2d4ce2147df68ca5b201405
Your Name <you@example.com>
2018-02-06 18:51:31 +0200

    My firs commit Yaaaaaa....

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+            echo "<h1>Noam</h1>";
+        ?>
+        
+        <form action="form.php">
+            First name:<br>
+            <input type="text" name="user" value="Mickey">
+            <br>
+            Last name:<br>
+            <input type="text" name="pwd" value="Mouse">
+            <br><br>
+            <input type="submit" value="Submit">
+          </form> 
+
+    </body>
+</html>
